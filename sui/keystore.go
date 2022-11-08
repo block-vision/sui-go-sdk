7d36280bbd625f34fb998e6df221bf2379e27251
jiang <neo@openboxfound.co>
2022-11-08 17:26:36 +0800

    feat: replace sync.Map with map
    feat: replace sync.Map with map

--- sui/keystore.go
+++ sui/keystore.go
@@ -2,9 +2,10 @@ package sui
 
 import (
 	"crypto/sha256"
+	"strings"
+
 	"github.com/block-vision/sui-go-sdk/common/sui_error"
 	"github.com/ledgerwatch/secp256k1"
-	"sync"
 
 	"github.com/block-vision/sui-go-sdk/common/keypair"
 	"github.com/block-vision/sui-go-sdk/config"
@@ -21,7 +22,7 @@ type IAccountKeyStore interface {
 
 type AccountKeyStoreImpl struct {
 	defaultAddress string
-	keystore       *sync.Map
+	keystore       map[string]models.SuiKeyPair
 }
 
 var accountStore *AccountKeyStoreImpl
@@ -32,7 +33,7 @@ func SetAccountKeyStore(configDir string) (IAccountKeyStore, error) {
 		return accountStore, err
 	}
 	accountStore = &AccountKeyStoreImpl{
-		keystore: new(sync.Map),
+		keystore: make(map[string]models.SuiKeyPair),
 	}
 	for i := range kpStrs.Keys {
 		if i == 0 {
@@ -42,7 +43,7 @@ func SetAccountKeyStore(configDir string) (IAccountKeyStore, error) {
 		if subErr != nil {
 			return accountStore, subErr
 		}
-		accountStore.keystore.Store(kp.Address, kp)
+		accountStore.keystore[strings.ToLower(kp.Address)] = kp
 	}
 	return accountStore, nil
 }
@@ -51,28 +52,27 @@ func (a *AccountKeyStoreImpl) Sign(address string, msg []byte) (signature []byte
 	if a.keystore == nil {
 		return []byte{}, sui_error.ErrNoKeyStoreInfo
 	}
-	kp, ok := a.keystore.Load(address)
+
+	kp, ok := a.keystore[strings.ToLower(address)]
 	if !ok {
 		return []byte{}, sui_error.ErrAddressNotInKeyStore
 	}
-	_keypair := kp.(models.SuiKeyPair)
-	if _keypair.Flag == keypair.Ed25519Flag {
-		return ed25519.Sign(ed25519.NewKeyFromSeed(_keypair.PrivateKey), msg), nil
+	if kp.Flag == keypair.Ed25519Flag {
+		return ed25519.Sign(ed25519.NewKeyFromSeed(kp.PrivateKey), msg), nil
 	}
 	data := sha256.Sum256(msg)
-	return secp256k1.Sign(data[:], _keypair.PrivateKey)
+	return secp256k1.Sign(data[:], kp.PrivateKey)
 }
 
 func (a *AccountKeyStoreImpl) GetKey(address string) (models.SuiKeyPair, error) {
 	if a.keystore == nil {
 		return models.SuiKeyPair{}, sui_error.ErrNoKeyStoreInfo
 	}
-	kp, ok := a.keystore.Load(address)
+	kp, ok := a.keystore[strings.ToLower(address)]
 	if !ok {
 		return models.SuiKeyPair{}, sui_error.ErrInvalidAddress
 	}
-	_keypair := kp.(models.SuiKeyPair)
-	return _keypair, nil
+	return kp, nil
 }
 
 func (a *AccountKeyStoreImpl) AddKey(key string) (err error) {
@@ -83,7 +83,7 @@ func (a *AccountKeyStoreImpl) AddKey(key string) (err error) {
 	if err != nil {
 		return err
 	}
-	a.keystore.Store(kp.Address, kp)
+	a.keystore[strings.ToLower(kp.Address)] = kp
 	return nil
 }
 
@@ -91,10 +91,9 @@ func (a *AccountKeyStoreImpl) Keys() (publicKeys []string) {
 	if a.keystore == nil {
 		return []string{}
 	}
-	a.keystore.Range(func(key, value any) bool {
-		_kp := value.(models.SuiKeyPair)
-		publicKeys = append(publicKeys, _kp.PublicKeyBase64)
-		return true
-	})
+	for _, kp := range a.keystore {
+		publicKeys = append(publicKeys, kp.PublicKeyBase64)
+
+	}
 	return publicKeys
 }
