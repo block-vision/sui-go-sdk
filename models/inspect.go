678b7b7b2b96b2dd393a993c7652567301a8ad3b
jiang <120166897+bvcrypto@users.noreply.github.com>
2023-01-18 16:31:27 +0800

    Add new data structure for new version

---
+++ models/inspect.go
@@ -0,0 +1,26 @@
+package models
+
+import "github.com/block-vision/sui-go-sdk/models/sui_json_rpc_types"
+
+type DevInsepctTransactionRequest struct {
+	TxBytes string
+}
+
+type DevInspectTransactionResponse struct {
+	Effects sui_json_rpc_types.SuiTransactionEffects `json:"effects"`
+	Results []interface{}                            `json:"results"`
+}
+
+type DevInspectMoveCallRequest struct {
+	SenderAddress   string
+	PackageObjectID string
+	Module          string
+	Function        string
+	TypeArguments   []string
+	Arguments       []interface{}
+}
+
+type DevInspectMoveCallResponse struct {
+	Effects sui_json_rpc_types.SuiTransactionEffects `json:"effects"`
+	Results []interface{}                            `json:"results"`
+}
